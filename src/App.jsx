721fe41d1fb17bd7450d82502075b2b3cb6d4608
Varshith Kumar Soma <varshithkumarsoma@gmail.com>
2025-01-13 23:26:31 +0530

    feat: added a navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,69 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import LeetCodeContests from "./components/LeetCodeContests";
-import "./App.css";
 import CodeForcesContests from "./components/CodeForcesContests";
 import CodeChefContests from "./components/CodeChefContests";
+import "./App.css";
+
 function App() {
+  const [selectedPlatform, setSelectedPlatform] = useState("CodeChef");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handlePlatformChange = (platform) => {
+    setSelectedPlatform(platform);
+    setIsLoading(true);
+
+    // Simulate API fetch time
+    setTimeout(() => {
+      setIsLoading(false); // Remove the loading state after fetching data
+    }, 1000); // Adjust this timeout to simulate fetch duration
+  };
+
   return (
-    <div>
+    <>
       <div className="parent">
         <Header />
         <h3>Next Coding Contests</h3>
         <p>Prepare yourself for the next set of exciting coding challenges.</p>
-        {/* <LeetCodeContests></LeetCodeContests> */}
-        {/* <CodeForcesContests></CodeForcesContests> */}
-        <CodeChefContests></CodeChefContests>
+        <div className="buttons">
+          <button
+            className={selectedPlatform === "CodeChef" ? "active" : ""}
+            onClick={() => handlePlatformChange("CodeChef")}
+          >
+            CodeChef
+          </button>
+          <button
+            className={selectedPlatform === "LeetCode" ? "active" : ""}
+            onClick={() => handlePlatformChange("LeetCode")}
+          >
+            LeetCode
+          </button>
+          <button
+            className={selectedPlatform === "CodeForces" ? "active" : ""}
+            onClick={() => handlePlatformChange("CodeForces")}
+          >
+            CodeForces
+          </button>
+        </div>
+
+        {isLoading ? (
+          <div className="loading">Loading...</div>
+        ) : (
+          <>
+            {selectedPlatform === "LeetCode" && <LeetCodeContests />}
+            {selectedPlatform === "CodeChef" && <CodeChefContests />}
+            {selectedPlatform === "CodeForces" && <CodeForcesContests />}
+            {selectedPlatform === "" && (
+              <>
+                <CodeChefContests />
+                <LeetCodeContests />
+                <CodeForcesContests />
+              </>
+            )}
+          </>
+        )}
       </div>
-    </div>
+    </>
   );
 }
 
--- src/components/CodeForcesComponent.jsx
+++ src/components/CodeForcesComponent.jsx
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import codeForcesLogo from "../assets/small-codeforces.png";
 import "./Contest.css";
 
 function CodeForcesComponent(props) {
+  const [timeLeft, setTimeLeft] = useState(0);
+  const formatTime = (time) => {
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((time % (1000 * 60)) / 1000);
+    return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+  };
+  const convertStartTimeToDate = (startTime) => {
+    const [date, time] = startTime.split(", ");
+    const [day, month, year] = date.split("/");
+    const [hours, minutes, seconds] = time.split(":");
+
+    return new Date(year, month - 1, day, hours, minutes, seconds);
+  };
+
+  useEffect(() => {
+    const contestTime = convertStartTimeToDate(props.startTime).getTime();
+
+    const timerInterval = setInterval(() => {
+      const currentTime = new Date().getTime();
+      const diff = contestTime - currentTime;
+
+      if (diff <= 0) {
+        clearInterval(timerInterval);
+      } else {
+        setTimeLeft(diff);
+      }
+    }, 1000);
+
+    return () => clearInterval(timerInterval);
+  }, [props.startTime]);
+
   const handleClick = (myLink) => () => {
     window.location.href = myLink;
   };
@@ -30,6 +63,11 @@ function CodeForcesComponent(props) {
               <td>Phase</td>
               <td>:</td>
               <td>{props.phase}</td>
+            </tr>
+            <tr>
+              <td>Countdown</td>
+              <td>:</td>
+              <td>{formatTime(timeLeft)}</td>
             </tr>
           </tbody>
         </table>
